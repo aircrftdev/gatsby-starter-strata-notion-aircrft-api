2d4e962bead98225104419c5386965c2fd3a23c2
aircrftdev <59378503+aircrftdev@users.noreply.github.com>
2021-04-02 13:50:55 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Layout from '../components/layout'
 class HomeIndex extends React.Component {
 
     render() {
-        const siteTitle = "Gatsby Starter - Strata"
+        const siteTitle = "Aircrft Blogging Engine"
         const siteDescription = "Site description"
 
         return (
@@ -21,16 +21,16 @@ class HomeIndex extends React.Component {
 
                     <section id="one">
                         <header className="major">
-                            <h2>This is a starter for Gatsby.js V2, demonstrating successful integration with notion as a CMS</h2>
+                            <h2>Welcome to your new blogging engine! Purpose built to make sharing what your working on at the moment, connecting with and growing a global community, a simply wonderful experience. </h2>
                         </header>
-                        <p>Based on the Strata site template, designed by HTML5 UP.<br />Integrated with <a href="https://conradlin.com/blog/posts/host-gatsbyjs-blog-with-notion-cms-and-netlify-for-free/" target="_blank">notion</a> as a CMS for demonstration purposes by <a href="https://conradlin.com" target="_blank">Conrad Lin</a></p>
+                        <p>Blogging is a great way to start learning what appeals to your audience. It is a relatively slow but inexpensive method of aggregating analytic data. Start by listing topics to write about, write consistently, and share your posts, articles, and newsletter across your business social media channels.<br />Get inspired and learn how harness your new superpower with our Build, Launch, Learn in Public Video Series<a href="https://blog.aircrft.com/" target="_blank">Aircrft blog</a> Watch this video for a demo on running your new blogging engine  <a href="https://aircrft.com" target="_blank">Aircrft Blog Lift-off</a></p>
                         <ul className="actions">
-                            <li><a href="https://www.youtube.com/watch?v=dMYKXVf88J0" target="_blank" className="button">Learn More</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=dMYKXVf88J0" target="_blank" className="button">Demo & Operating Manual</a></li>
                         </ul>
                     </section>
 
                     <section id="two">
-                        <h2>Recent Sharing</h2>
+                        <h2>Activity Feeds</h2>
 
                         <ul className="actions">
                             <li><Link to={`blog`} className="button">All Articles</Link></li>
@@ -40,7 +40,7 @@ class HomeIndex extends React.Component {
 
                     <section id="three">
                         <h2>Get In Touch</h2>
-                        <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
+                        <p>Need additional support? Will ❤ to help! Get in touch</p>
                         <div className="row">
                             <div className="8u 12u$(small)">
                                 <form method="post" action="#">
@@ -58,17 +58,17 @@ class HomeIndex extends React.Component {
                                 <ul className="labeled-icons">
                                     <li>
                                         <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                        1234 Somewhere Rd.<br />
-                                        Nashville, TN 00000<br />
+                                        Handcrafted with Love<br />
+                                        Los Angleges CA<br />
                                         United States
                                     </li>
                                     <li>
                                         <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                                        000-000-0000
+                                        323-527-8352
                                     </li>
                                     <li>
                                         <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
-                                        <a href="#">lets.talk@conradlin.com</a>
+                                        <a href="#">support@aircrft.com</a>
                                     </li>
                                 </ul>
                             </div>
